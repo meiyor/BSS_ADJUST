8b7a0a23a30eeaf4851bb1dbab854f97f1e63fd6
Juan Manuel Mayor Torres <juan.mayortorres@unitn.it>
2021-07-07 09:02:12 -0500

    Update reading_gtec_EKSO_mode.m

--- reading_gtec_EKSO_mode.m
+++ reading_gtec_EKSO_mode.m
@@ -1,6 +1,8 @@
 function reading_gtec_EKSO_mode(file_path,SUBJ)
+
+%% include the filepath to the EKSO file you are provided and the suffix for saving the final results in SUBJ
 close all;
-rmpath(genpath('/home/jmm_vivobook_asus/DeepGaze_project/eeglab14_1_1b'));
+rmpath(genpath('/path_to_EEGLab/eeglab14_1_1b'));
 DATA=load(file_path);
 data_accel=DATA.data(:,[9:11 15:17]);
 for k=1:size(data_accel,2)
@@ -13,9 +15,10 @@ plot(t,DATA_ref,'LineWidth',2);
 hold on;
 [point_a,time_a]=findpeaks(DATA_ref,t,'MinPeakDistance',1.5,'MinPeakHeight',0.8);
 plot(time_a,point_a,'x','MarkerSize',10,'LineWidth',2);
-addpath(genpath('/home/jmm_vivobook_asus/DeepGaze_project/eeglab14_1_1b'));
-rmpath(genpath('/home/jmm_vivobook_asus/DeepGaze_project/eeglab14_1_1b/functions/octavefunc'));
-%% segment the signal
+addpath(genpath('/path_to_EEGLab/eeglab14_1_1b'));
+rmpath(genpath('/path_to_EEGLab/eeglab14_1_1b/functions/octavefunc'));
+
+%% segment the signal and process artifact rejection on each segmented trial
 n=1;
 for l=1:2:length(time_a)-2
     pos1=min(find(t>=time_a(l)));
@@ -29,7 +32,7 @@ for l=1:2:length(time_a)-2
     %% ADJUST application
     EEGdata{n}.icawinv=inv(EEGdata{n}.icaweights*EEGdata{n}.icasphere);
     EEGdata{n}.icachansind=[1:1:64];
-    rmpath(genpath('/home/jmm_vivobook_asus/DeepGaze_project/eeglab14_1_1b/fieldtrip-20210507'));
+    rmpath(genpath('/path_to_EEGLab/eeglab14_1_1b/fieldtrip-20210507'));
     EEGdata{n}=cleanline('EEG',EEGdata{n},'Bandwidth',1,'ChanCompIndices',[1:EEGdata{n}.nbchan],'SignalType','Channels','ComputeSpectralPower','true','LineFrequencies',[50]);
     addpath(genpath('/home/jmm_vivobook_asus/DeepGaze_project/eeglab14_1_1b/fieldtrip-20210507'));
     EEG_no{n}=EEGdata{n};
@@ -46,13 +49,15 @@ for l=1:2:length(time_a)-2
     close all;
     n=n+1;
 end;
-rmpath(genpath('/home/jmm_vivobook_asus/DeepGaze_project/eeglab14_1_1b'));
+
+rmpath(genpath('/path_to_EEGLab/eeglab14_1_1b'));
 for i=1:size(EEG_spec,1)
     for l=1:size(EEG_spec,2)
         r_spec(i,l)=abs(thd(squeeze(EEG_spec(i,l,1:101))',linspace(0,100,101)',3,'psd'));
         r_no_spec(i,l)=abs(thd(squeeze(EEG_no_spec(i,l,1:101))',linspace(0,100,101)',3,'psd'));
     end;
 end;
+
 index=r_spec==Inf;
 r_spec(index==1)=0;
 index=r_spec==-Inf;
@@ -63,6 +68,9 @@ index=r_no_spec==-Inf;
 r_no_spec(index==1)=0;
 Val_no=nanmean(r_no_spec,2)';
 Val_spec=nanmean(r_spec,2)';
+
+%% plotting and statistical evaluation
 notBoxPlot([Val_no Val_spec],[zeros([1,length(Val_no)]),ones([1,length(Val_spec)])],'style','sdline');
+anova1([Val_no Val_spec],[zeros([1,length(Val_no)]),ones([1,length(Val_spec)])])
 save(['res_gtec_clean_' SUBJ '.mat' ],'r_spec','r_no_spec','Val_no','Val_spec','EEG_spec','EEG_no_spec','EEGdata','EEG_no');
 A=1;
