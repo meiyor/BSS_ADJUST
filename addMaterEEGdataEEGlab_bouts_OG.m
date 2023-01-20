99fafeded74079afcddb737f9bae88da2feda554
Juan Manuel Mayor Torres <juan.mayortorres@unitn.it>
2023-01-20 21:34:34 +0000

    Add files via upload

--- addMaterEEGdataEEGlab_bouts_OG.m
+++ addMaterEEGdataEEGlab_bouts_OG.m
@@ -141,8 +141,8 @@ while ind==0
          EEGR_set{ccount}.nbchan=EEGchanR;
          end;
          
-         [EEGL_set{ccount}.icaweights,EEGL_set{ccount}.icasphere]=runica(EEGL_set{ccount}.data(:,:),'sphering','on','lrate',1e-5,'maxsteps',50); 
-         [EEGR_set{ccount}.icaweights,EEGR_set{ccount}.icasphere]=runica(EEGR_set{ccount}.data(:,:),'sphering','on','lrate',1e-5,'maxsteps',50); 
+         [EEGL_set{ccount}.icaweights,EEGL_set{ccount}.icasphere]=runica(EEGL_set{ccount}.data(:,:),'sphering','on','lrate',1e-5,'maxsteps',100,'reset_randomseed','off'); 
+         [EEGR_set{ccount}.icaweights,EEGR_set{ccount}.icasphere]=runica(EEGR_set{ccount}.data(:,:),'sphering','on','lrate',1e-5,'maxsteps',100,'reset_randomseed','off'); 
          delete('report_left_n.txt');
          delete('report_right_n.txt');
          %% implement ADJUST here
