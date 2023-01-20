f9b6618642bfb4f9085e35569c1d92f1363982cb
Juan Manuel Mayor Torres <juan.mayortorres@unitn.it>
2023-01-20 21:02:08 +0000

    Add files via upload

--- addMaterEEGdataEEGlab_bouts_OG.m
+++ addMaterEEGdataEEGlab_bouts_OG.m
@@ -1,8 +1,8 @@
-function addMaterEEGdataEEGlab_bouts(subj,runind,size_resamp,sel_emg)
-%% add the EEGlab path here to employ EEG signal preprocessing if necessary %%
-%addpath(genpath('/home/jmm_vivobook_asus/DeepGaze_project/eeglab14_1_1b'));%%%% add path eeglab
+function addMaterEEGdataEEGlab_bouts_OG(subj,runind,size_resamp,sel_emg)
+%% add the EEGlab path here to employ EEG signal preprocessing
+%addpath(genpath('/home/jmm_vivobook_asus/DeepGaze_project/eeglab14_1_1b')); %%% add path eeglab
 %
-%rmpath('/home/jmm_vivobook_asus/DeepGaze_project/eeglab14_1_1b/field_trip'); %%%% remove field_trip from eeglab
+%rmpath('/home/jmm_vivobook_asus/DeepGaze_project/eeglab14_1_1b/field_trip/'); %%% remove field_trip from eeglab
 ind=0;
 ccount=1;
 ckcount=1;
@@ -14,15 +14,15 @@ end;
 while ind==0
      EEGL=eeg_emptyset();
      EEGR=eeg_emptyset();
-     [accel{ccount},EEG{ccount},ind,indbout,EMG_sal{ccount}]=read_subjects_EEG_Mater_alt(subj,runind,ckcount,1,0,1,sel_emg,5,EMG.syncData); %% processing the window on 5 seconds around the Bout detection
+     [accel{ccount},EEG{ccount},ind,indbout,EMG_sal{ccount}]=read_subjects_EEG_Mater_alt_OG(subj,runind,ckcount,1,0,1,sel_emg,2.2,EMG.syncData); %% processing the window on 3 seconds around the Bout detection
      %ind
      if ind==1
          %save([subj '_' runind '_res_vals.mat'],'erspl','itcpl','freql','erspr','itcpr','freqr','tl','tr');
+         %% cutting the possible bouts
          break;
      end;
      EEGL_set{ccount}=EEGlabstructure_def(EEGL,EEG{ccount}{2,1}',EEG{ccount}{4,1},EEG{ccount}{6,1},EEG{ccount}{1,1},subj,runind);
      EEGR_set{ccount}=EEGlabstructure_def(EEGR,EEG{ccount}{2,2}',EEG{ccount}{4,2},EEG{ccount}{6,2},EEG{ccount}{1,2},subj,runind);
-     
      if indbout==0
          EEGL_set{ccount} = pop_eegfiltnew(EEGL_set{ccount},0.1,100,8250);
          EEGR_set{ccount} = pop_eegfiltnew(EEGR_set{ccount},0.1,100,8250);
@@ -34,13 +34,6 @@ while ind==0
                     t_data=detrend(t_data);
                     EEGR_set{ccount}.data(k,:)=t_data(2/2:EEGR_set{ccount}.pnts+(2/2-1));
          end;
-         
-         baseline_channame_L={EEGL_set{ccount}.chanlocs(:).labels};
-         baseline_chanloc_L=EEGL_set{ccount}.chanlocs;
-         
-         baseline_channame_R={EEGR_set{ccount}.chanlocs(:).labels};
-         baseline_chanloc_R=EEGR_set{ccount}.chanlocs;
-         
          EEGL_set{ccount}=pop_autobssemg(EEGL_set{ccount},1,1,'bsscca',{'eigratio',1e6},'emg_psd',{'ratio',10,'fs',250,'range',[1 10]});
          EEGR_set{ccount}=pop_autobssemg(EEGR_set{ccount},1,1,'bsscca',{'eigratio',1e6},'emg_psd',{'ratio',10,'fs',250,'range',[1 10]});
          EEGL_set{ccount}=clean_rawdata(EEGL_set{ccount},5,[0.25 0.75],0.85,-1,-1,-1);
@@ -147,7 +140,6 @@ while ind==0
          EEGR_set{ccount}.data=temp;
          EEGR_set{ccount}.nbchan=EEGchanR;
          end;
-
          
          [EEGL_set{ccount}.icaweights,EEGL_set{ccount}.icasphere]=runica(EEGL_set{ccount}.data(:,:),'sphering','on','lrate',1e-5,'maxsteps',50); 
          [EEGR_set{ccount}.icaweights,EEGR_set{ccount}.icasphere]=runica(EEGR_set{ccount}.data(:,:),'sphering','on','lrate',1e-5,'maxsteps',50); 
@@ -157,7 +149,7 @@ while ind==0
          %% left
          EEGL_set{ccount}.icawinv=inv(EEGL_set{ccount}.icaweights*EEGL_set{ccount}.icasphere);
          EEGL_set{ccount}.data= repmat(EEGL_set{ccount}.data(:,:),1,1,25);
-         EEGL_set{ccount}.trials=25;
+         EEGL_set{ccount}.trials=100; %% 25 repetitions
          EEGL_set{ccount}.icachansind=[1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32];
          [art_channels_left]=ADJUST(EEGL_set{ccount},'report_left_n.txt');
          EEGL_set{ccount}=pop_subcomp(EEGL_set{ccount},art_channels_left);
@@ -165,7 +157,7 @@ while ind==0
          %% right
          EEGR_set{ccount}.icawinv=inv(EEGR_set{ccount}.icaweights*EEGR_set{ccount}.icasphere);
          EEGR_set{ccount}.data= repmat(EEGR_set{ccount}.data(:,:),1,1,25);
-         EEGR_set{ccount}.trials=25;
+         EEGR_set{ccount}.trials=100;%% 25 repetitions
          EEGR_set{ccount}.icachansind=[1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32];
          delete('report_left_n.txt');
          delete('report_right_n.txt');
@@ -184,20 +176,19 @@ while ind==0
          if tpr(ccount)>=10.5
             tpr(ccount)=0;
          end;
-         
-    close all;
-    if ~isreal(EEGL_set{ccount}.data(:,:))
-      EEGL_set{ccount}.data(:,:)=real(EEGL_set{ccount}.data(:,:));
-    end;
-    if ~isreal(EEGR_set{ccount}.data(:,:))
-      EEGR_set{ccount}.data(:,:)=real(EEGR_set{ccount}.data(:,:));
-    end;
-         
-    for ch=1:32
+         ccount
+         for ch=1:32
               close all;
-              [erspl{ccount,ch},itcpl{ccount,ch},powbasel{ccount,ch},timescl{ccount,ch},freql{ccount,ch}]=newtimef(EEGL_set{ccount}.data(ch,:),EEGL_set{ccount}.pnts,[EEGL_set{ccount}.xmin EEGL_set{ccount}.xmax]*1000,EEGL_set{ccount}.srate,0,'winsize',50,'nfreqs',1000,'freqs',[0 50],'padratio',32,'plotersp','off','plotitc','off');
-              [erspr{ccount,ch},itcpr{ccount,ch},powbaser{ccount,ch},timescr{ccount,ch},freqr{ccount,ch}]=newtimef(EEGR_set{ccount}.data(ch,:),EEGR_set{ccount}.pnts,[EEGR_set{ccount}.xmin EEGR_set{ccount}.xmax]*1000,EEGR_set{ccount}.srate,0,'winsize',50,'nfreqs',1000,'freqs',[0 50],'padratio',32,'plotersp','off','plotitc','off');
+              if ~isreal(EEGL_set{ccount}.data(ch,:))
+                  EEGL_set{ccount}.data(ch,:)=real(EEGL_set{ccount}.data(ch,:));
+              end;
+              if ~isreal(EEGR_set{ccount}.data(ch,:))
+                  EEGR_set{ccount}.data(ch,:)=real(EEGR_set{ccount}.data(ch,:));
+              end;
+              [erspl{ccount,ch},itcpl{ccount,ch},powbasel{ccount,ch},timescl{ccount,ch},freql{ccount,ch}]=newtimef(EEGL_set{ccount}.data(ch,:).^2,EEGL_set{ccount}.pnts,[EEGL_set{ccount}.xmin EEGL_set{ccount}.xmax]*1000,EEGL_set{ccount}.srate,0,'winsize',50,'nfreqs',1000,'freqs',[0 50],'padratio',32,'plotersp','off','plotitc','off');
+              [erspr{ccount,ch},itcpr{ccount,ch},powbaser{ccount,ch},timescr{ccount,ch},freqr{ccount,ch}]=newtimef(EEGR_set{ccount}.data(ch,:).^2,EEGR_set{ccount}.pnts,[EEGR_set{ccount}.xmin EEGR_set{ccount}.xmax]*1000,EEGR_set{ccount}.srate,0,'winsize',50,'nfreqs',1000,'freqs',[0 50],'padratio',32,'plotersp','off','plotitc','off');
               %[erspr{ccount,ch},itcpr{ccount,ch},powbaser{ccount,ch},timescr{ccount,ch},freqr{ccount,ch}]=newtimef(EEGR_set{ccount}.data(ch,:),EEGR_set{ccount}.pnts,[EEGR_set{ccount}.xmin EEGR_set{ccount}.xmax]*1000,EEGR_set{ccount}.srate,0,'itctype','phasecoher','winsize',40,'freqs',[0.1 20],'nfreqs',250,'padratio',16,'plotersp','off','plotitc','off');
+              ccount
               if sel_emg==1
                   [PL1{ccount,ch},fL1{ccount}]=pwelch(squeeze(EEGL_set{ccount}.data(ch,:))./max(squeeze(EEGL_set{ccount}.data(ch,:))),squeeze(EEGL_set{ccount}.data(ch,:))./max(squeeze(EEGL_set{ccount}.data(ch,:))),250/2,1000,250,'psd','onesided');
                   [PR1{ccount,ch},fR1{ccount}]=pwelch(squeeze(EEGR_set{ccount}.data(ch,:))./max(squeeze(EEGR_set{ccount}.data(ch,:))),squeeze(EEGR_set{ccount}.data(ch,:))./max(squeeze(EEGR_set{ccount}.data(ch,:))),250/2,1000,250,'psd','onesided');
@@ -286,8 +277,9 @@ while ind==0
                   coh_TA_Rs{ccount,ch}=(abs(SR2_TA{ccount,ch}).^2)./((SR1{ccount,ch})*(SR3_TA{ccount}));
                   coh_RF_Ls{ccount,ch}=(abs(SL2_RF{ccount,ch}).^2)./((SL1{ccount,ch})*(SL3_RF{ccount}));
                   coh_RF_Rs{ccount,ch}=(abs(SR2_RF{ccount,ch}).^2)./((SR1{ccount,ch})*(SR3_RF{ccount}));
-              end;
+               end;
          end;
+         %save([subj '_' runind '_res_vals.mat'],'erspl','itcpl','powbasel','timescl','freql','erspr','itcpr','powbaser','timescr','freqr','tl','tr');
          ccount=ccount+1;
      else
          accel{ccount}=[];
@@ -297,5 +289,5 @@ while ind==0
      end;
      ckcount=ckcount+1;
 end;
-save([subj '_' runind '_res_vals.mat'],'erspl','itcpl','freql','erspr','itcpr','freqr','tl','tr','accel','thl','thr','tpl','tpr');
-save([subj '_' runind '_res_coherence.mat'],'coh_SO_L','coh_SO_R','coh_ST_L','coh_ST_R','coh_TA_L','coh_TA_R','coh_RF_L','coh_RF_R','coh_SO_Ls','coh_SO_Rs','coh_ST_Ls','coh_ST_Rs','coh_TA_Ls','coh_TA_Rs','coh_RF_Ls','coh_RF_Rs','PL1','PR1','PL2_SO','PR2_SO','PL2_ST','PR2_ST','PL2_RF','PR2_RF','PL3_SO','PR3_SO','PL3_ST','PR3_ST','PL3_TA','PR3_TA','PL3_RF','PR3_RF');
+save([subj '_' runind '_res_vals_OG.mat'],'erspl','itcpl','freql','erspr','itcpr','freqr','tl','tr','accel','thl','thr','tpl','tpr');
+save([subj '_' runind '_res_coherence_OG.mat'],'coh_SO_L','coh_SO_R','coh_ST_L','coh_ST_R','coh_TA_L','coh_TA_R','coh_RF_L','coh_RF_R','coh_SO_Ls','coh_SO_Rs','coh_ST_Ls','coh_ST_Rs','coh_TA_Ls','coh_TA_Rs','coh_RF_Ls','coh_RF_Rs','PL1','PR1','PL2_SO','PR2_SO','PL2_ST','PR2_ST','PL2_RF','PR2_RF','PL3_SO','PR3_SO','PL3_ST','PR3_ST','PL3_TA','PR3_TA','PL3_RF','PR3_RF');
